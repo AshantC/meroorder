c09ca1439c56c0bb6199e8154f39f96401b9af7f
AshantC <ashantchaudhary22@gmail.com>
2024-04-01 14:02:43 +0545

    initial commit

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Aboutus = () => {
+    return (
+        <div>
+            <h1 className='text-center font-extrabold text-4xl bg-black text-white h-14 py-2'>Mero Order</h1>
+            <div className='text-center mt-2'>
+                <h1 className='font-semibold underline text-xl'>Team Member</h1>
+                <div className='text-gray-700 mt-2'>
+                    <h2>Ashant Chaudhary</h2>
+                    <h2>Prajesh Kangoy</h2>
+                    <h2>Jyoti Sardar</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Aboutus
